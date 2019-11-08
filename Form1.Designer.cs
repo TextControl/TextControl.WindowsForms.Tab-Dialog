849d221d897ba508d80d662cfa6106fb8e9a09b1
Bjoern Meyer <bjoern@textcontrol.com>
2019-11-08 21:18:13 +0100

    Initial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace txTabPositions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textControl1 = new TXTextControl.TextControl();
+            this.rulerBar1 = new TXTextControl.RulerBar();
+            this.SuspendLayout();
+            // 
+            // textControl1
+            // 
+            this.textControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textControl1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textControl1.Location = new System.Drawing.Point(0, 25);
+            this.textControl1.Name = "textControl1";
+            this.textControl1.RulerBar = this.rulerBar1;
+            this.textControl1.Size = new System.Drawing.Size(812, 397);
+            this.textControl1.TabIndex = 0;
+            this.textControl1.Text = "textControl1";
+            this.textControl1.UserNames = null;
+            // 
+            // rulerBar1
+            // 
+            this.rulerBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.rulerBar1.Location = new System.Drawing.Point(0, 0);
+            this.rulerBar1.Name = "rulerBar1";
+            this.rulerBar1.Size = new System.Drawing.Size(812, 25);
+            this.rulerBar1.TabIndex = 1;
+            this.rulerBar1.Text = "rulerBar1";
+            this.rulerBar1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.rulerBar1_MouseDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 422);
+            this.Controls.Add(this.textControl1);
+            this.Controls.Add(this.rulerBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private TXTextControl.TextControl textControl1;
+        private TXTextControl.RulerBar rulerBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txTabPositions
+{
+    public partial class Form1 : Form
+    {
+        private int iDpiX = 15;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            // get resolution conversion value
+            Graphics g = textControl1.CreateGraphics();
+            iDpiX = (int)(1440 / g.DpiX);
+
+            // set page units to twips
+            textControl1.PageUnit = TXTextControl.MeasuringUnit.Twips;
+        }
+
+        private void rulerBar1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // calculate zoom factor
+            float fZoomFactor = (float)textControl1.ZoomFactor / 100;
+
+            // get the click location in the ruler bar
+            int iClickLocation = (int)((e.X * iDpiX) + (textControl1.ScrollLocation.X * fZoomFactor));
+
+            // loop through all tab positions
+            foreach (int tabPosition in textControl1.Paragraphs.GetItem(
+                textControl1.Selection.Start).Format.TabPositions)
+            {
+                // retrieve the exact location including zoom factor
+                int iTabLocation = (int)((tabPosition + 
+                    textControl1.Sections.GetItem().Format.PageMargins.Left) * fZoomFactor);
+
+                // if click location matches tab position
+                // open the tab dialog
+                if (iClickLocation >= iTabLocation - 50 && iClickLocation <= iTabLocation + 50)
+                {
+                    textControl1.TabDialog(); // open dialog
+                    break;
+                }
+            }
+        }
+    }
+}
